879341ba6041fe4357108bdbf57a7cf27c6ae9f5
stephenegbuna <emeka.egbuna@city.ac.uk>
2021-12-23 18:25:34 +0000

    wsdl

---
+++ src/main/java/wsdl/Apps.java
@@ -0,0 +1,13 @@
+package wsdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apps {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apps.class, args);
+    }
+
+}
